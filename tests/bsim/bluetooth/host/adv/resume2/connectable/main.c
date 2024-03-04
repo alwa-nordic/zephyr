9ca6d47127e8549e4996bf437181bfb03c5f9b0c
Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>
2024-03-04 15:08:06 +0100

    Bluetooth: tests: bsim: host/adv/resume2
    This test is an attempt at formalizing at least part of the behavior
    described in commit 372c8f2d92fce3653f5024261e17012197acc330.
    
    Signed-off-by: Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>

---
+++ tests/bsim/bluetooth/host/adv/resume2/connectable/main.c
@@ -0,0 +1,36 @@
+/* Copyright (c) 2024 Nordic Semiconductor ASA
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <testlib/adv.h>
+#include <testlib/conn.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/conn.h>
+#include <zephyr/sys/__assert.h>
+
+int main(void)
+{
+	int err;
+
+	err = bt_enable(NULL);
+	__ASSERT_NO_MSG(!err);
+
+	err = bt_set_name("connectable");
+	__ASSERT_NO_MSG(!err);
+
+	while (true) {
+		struct bt_conn *conn = NULL;
+
+		bt_testlib_conn_wait_free();
+
+		err = bt_testlib_adv_conn(
+			&conn, BT_ID_DEFAULT,
+			(BT_LE_ADV_OPT_USE_NAME | BT_LE_ADV_OPT_FORCE_NAME_IN_AD));
+		__ASSERT_NO_MSG(!err);
+
+		bt_testlib_wait_disconnected(conn);
+		bt_testlib_conn_unref(&conn);
+	}
+
+	return 0;
+}

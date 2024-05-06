1b46bd42d2f9f57b88ef0b244fd951c51df62f1a
Aleksander Wasaznik <aleksander.wasaznik@nordicsemi.no>
2024-05-06 18:27:10 +0200

    wip

--- subsys/bluetooth/host/dev_rl.c
+++ subsys/bluetooth/host/dev_rl.c
@@ -6,6 +6,7 @@
 
 #include "keys.h"
 
+#include "syscalls/kernel.h"
 #include "zephyr/kernel.h"
 #include "zephyr/sys/atomic.h"
 #include "zephyr/sys/slist.h"
@@ -19,7 +20,7 @@ enum bt_dev_rl_read_lock_t {
 
 struct bt_dev_rl_add_async_t {
 	sys_snode_t __node;
-	struct bt_hci_cp_le_add_dev_to_rl *entry;
+	const struct bt_hci_cp_le_add_dev_to_rl *entry;
 };
 
 struct bt_dev_rl_del_async_t {
@@ -28,19 +29,34 @@ struct bt_dev_rl_del_async_t {
 };
 
 atomic_t bt_dev_rl_lock;
+
+/* Short-locking mutex to protect both the signal and the
+ * sys_slist, which has a thread-unsafe implementation.
+ *
+ * This can be a spinlock instead.
+ */
+struct k_mutex bt_dev_rl_mutex;
+
+/* sys_slist operations are not thread-safe */
 sys_slist_t bt_dev_rl_ops_del;
 sys_slist_t bt_dev_rl_ops_add;
 
-void bt_dev_rl_ops_add_add(struct bt_dev_rl_add_async_t *op)
-{
-	sys_slist_append(&bt_dev_rl_ops_add, &op->__node);
-}
+/* Atomic invariant protected by `bt_dev_rl_mutex`: The signal
+ * is never observed raised while either of the sys_slist have
+ * any items.
+ */
+struct k_poll_signal bt_dev_rl_idle;
+
 
 struct bt_dev_rl_add_async_t *bt_dev_rl_ops_add_dequeue(void)
 {
 	sys_snode_t *node;
 
+	/* Mutex protects unsafe implementation `sys_slist_get`. */
+	k_mutex_lock(&bt_dev_rl_mutex, K_FOREVER);
 	node = sys_slist_get(&bt_dev_rl_ops_add);
+	k_mutex_unlock(&bt_dev_rl_mutex);
+
 	if (!node) {
 		return NULL;
 	}
@@ -48,33 +64,37 @@ struct bt_dev_rl_add_async_t *bt_dev_rl_ops_add_dequeue(void)
 	return CONTAINER_OF(node, struct bt_dev_rl_add_async_t, __node);
 }
 
-struct k_poll_signal bt_dev_rl_write_done;
-
 void bt_hci_cmd_gen_req(void);
 void bt_dev_rl_start_write() {
-	k_poll_signal_reset(&bt_dev_rl_write_done);
 	atomic_set_bit(&bt_dev_rl_lock, BT_DEV_RL_LOCK_WRITE);
 	bt_hci_cmd_gen_req();
 }
 
 void bt_dev_rl_add_async(struct bt_dev_rl_add_async_t *op)
 {
-	/* Append is O(1) */
-	bt_dev_rl_ops_add_add(op);
+	k_mutex_lock(&bt_dev_rl_mutex, K_FOREVER);
+
+	/* Not thread-safe */
+	sys_slist_append(&bt_dev_rl_ops_add, &op->__node);
+
+	/* Signal shall not be observed high while items are pending.
+	 */
+	k_poll_signal_reset(&bt_dev_rl_idle);
+
 	bt_dev_rl_start_write();
+
+	k_mutex_unlock(&bt_dev_rl_mutex);
 }
 
 /* Legacy support */
-int bt_dev_rl_add_sync(const bt_addr_le_t *peer_id, struct bt_irk *peer_irk, struct bt_irk *local_irk)
+int bt_dev_rl_add_sync(const struct bt_hci_cp_le_add_dev_to_rl *entry)
 {
 	struct bt_dev_rl_add_async_t op = {
-		.entry.peer_id = peer_id,
-		.entry.peer_irk = peer_irk,
-		.entry.local_irk = local_irk,
+		.entry = entry,
 	};
 
 	bt_dev_rl_add_async(&op);
-	k_poll_signal_wait(&bt_dev_rl_write_done);
+	k_poll_signal_wait(&bt_dev_rl_idle);
 
 	return 0;
 }
@@ -86,15 +106,10 @@ int bt_dev_rl_del_sync(const bt_addr_le_t *peer_id)
 }
 
 void bt_hci_cmd_set_hdr(struct net_buf *buf, uint16_t opcode, uint8_t param_len);
-void bt_dev_rl_hci_cmd_fill_add(struct net_buf *buf, const bt_addr_le_t *peer_id, struct bt_irk *peer_irk, struct bt_irk *local_irk)
+void bt_dev_rl_hci_cmd_fill_add(struct net_buf *buf, const struct bt_hci_cp_le_add_dev_to_rl *entry)
 {
-	struct bt_hci_cp_le_add_dev_to_rl *cp;
-
-	bt_hci_cmd_set_hdr(buf, BT_HCI_OP_LE_ADD_DEV_TO_RL, sizeof(*cp));
-	cp = net_buf_add(buf, sizeof(*cp));
-	bt_addr_le_copy(&cp->peer_id_addr, peer_id);
-	memcpy(cp->peer_irk, peer_irk, 16);
-	memcpy(cp->local_irk, local_irk, 16);
+	bt_hci_cmd_set_hdr(buf, BT_HCI_OP_LE_ADD_DEV_TO_RL, sizeof(*entry));
+	net_buf_add_mem(buf, entry, sizeof(*entry));
 }
 
 int bt_dev_rl_dequeue_cmd(struct net_buf *buf)
@@ -102,7 +117,7 @@ int bt_dev_rl_dequeue_cmd(struct net_buf *buf)
 	struct bt_dev_rl_add_async_t *add_op;
 
 	if ((add_op = bt_dev_rl_ops_add_dequeue())) {
-		bt_dev_rl_hci_cmd_fill_add(buf, add_op->peer_id, add_op->peer_irk, add_op->local_irk);
+		bt_dev_rl_hci_cmd_fill_add(buf, add_op->entry);
 		return 0;
 	}
 
